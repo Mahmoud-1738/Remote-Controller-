75ed32f6b12db04e0cd3a419afd3ea0b0ee68756
Mahmoud-1764 <mahmoudchuaibsuliman@gmail.com>
2026-03-31 23:16:55 +0200

    game worked

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
+const os = require('os');
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, '../game')));
 
-   res.json({ message: 'Hello World!' });
+function getLanIp() {
+    const nets = os.networkInterfaces();
+    for (const ifaces of Object.values(nets)) {
+        for (const iface of ifaces) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return 'localhost';
+}
+
+const lanIp = getLanIp();
+const controllerUrl = `http://${lanIp}:${port}/controller.html`;
+
+io.on('connection', (socket) => {
+    socket.emit('serverInfo', { controllerUrl });
+
+    socket.on('input', (data) => {
+        socket.broadcast.emit('gameInput', data);
+    });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+server.listen(port, () => {
+    console.log(`Game:       http://localhost:${port}`);
+    console.log(`Controller: ${controllerUrl}`);
 });
